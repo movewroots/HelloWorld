40cc21c076285e4a8e7729c1fa9ff7df63f61f1b
movewroots <movinonwithroots@gmail.com>
2022-10-18 23:07:15 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nikita Verkhoturov on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
